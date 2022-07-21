6fbff69d05acc05e86d9f8f180d3dba3ed12819d
Loc Mai <locmai0201@gmail.com>
2022-07-22 00:58:10 +0700

    I dunno cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,15 +10,27 @@ void setup(void) {
     tft.fillScreen(TFT_BLACK);
 
     // Set "cursor" at top left corner of display (0,0) and select font 4
-    tft.setCursor(0, 0, 4);
+    tft.setCursor(10, 0, 4);
 
     // Set the font colour to be white with a black background
     tft.setTextColor(TFT_WHITE, TFT_BLACK);
+
+    
 }
 
 void loop() {
     tft.fillScreen(TFT_BLACK);
-    tft.setCursor(0, 0, 4);
-    tft.println("Hello, world!\n");
+    tft.setTextColor(TFT_GREEN,TFT_BLACK);
+    tft.setCursor(10, 0, 4);
+    tft.println("Hello, World");
+
+    tft.setCursor(1, 210);
+    tft.print("[");
+    tft.setTextSize(1);
+    for (int i = 0; i < 10; i++) {
+        tft.print(".");
+    }
+    tft.setCursor(230, 210, 4);
+    tft.print("]");
     delay(5000);
 }
